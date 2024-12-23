97f15321fdd8f7dbf8d62b5030e0c18ba3e17637
nishtha1010 <nishthapatel1012@gmail.com>
2024-12-23 16:59:20 +0530

    Contactus Page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "../src/pages/SignupPage";
 import ProfileSignup from "./components/signupComponents/ProfileSignup";
-
+import Contact from "./pages/Contact";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/profilesignup" element={<ProfileSignup />} />
+        <Route path="/contact" element={<Contact />} />
+        
       </Routes>
     </BrowserRouter>
   );
