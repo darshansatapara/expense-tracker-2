ef944f85e5ec3f99f873de95c7acc0de73defef8
darshansatapara <darshansatapara286@gmail.com>
2025-05-05 22:54:42 +0530

    headers changes in axios

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -4,5 +4,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.LIVE_URL || "http://localhost:5000/api",
   withCredentials: true,
-
+  headers: {
+    "Content-Type": "application/json" || "text/html",
+  },
 });
