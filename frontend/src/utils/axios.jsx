171f22c96374280c190ef1b9eb1732f521e1bc38
darshansatapara <darshansatapara286@gmail.com>
2025-05-05 22:52:03 +0530

    another chnage in the axios

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -4,7 +4,5 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.LIVE_URL || "http://localhost:5000/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+
 });
