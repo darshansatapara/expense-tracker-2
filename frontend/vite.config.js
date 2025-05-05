64f860420fe61fcd0a9fcb54edc923f65fec0690
darshansatapara <darshansatapara286@gmail.com>
2025-05-05 21:40:18 +0530

    changes in the vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,31 +5,30 @@ import dotenv from "dotenv";
 // Load environment variables from .env filenbvhvjh
 dotenv.config();
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:5000",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      external: [
-        "mongoose", // Exclude mongoose
-        /backend\/.*/, // Exclude backend files
-      ],
-    },
-    sourcemap: true,
-  },
-  define: {
-    "process.env": {},
-  },
+  base: "/",
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: process.env.VITE_API_URL || "http://localhost:5000",
+  //       changeOrigin: true,
+  //       secure: true,
+  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
+  //     },
+  //   },
+  // },
+  // build: {
+  //   outDir: "dist",
+  //   rollupOptions: {
+  //     external: [
+  //       "mongoose", // Exclude mongoose
+  //       /backend\/.*/, // Exclude backend files
+  //     ],
+  //   },
+  //   sourcemap: true,
+  // },
+  // define: {
+  //   "process.env": {},
+  // },
 });
