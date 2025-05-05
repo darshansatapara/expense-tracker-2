3164e9fe9b5fb85391704cebd4b84135cc97687e
darshansatapara <darshansatapara286@gmail.com>
2025-05-05 22:39:48 +0530

    changes in every file of axios

--- frontend/src/store/UserStore/userCategoryStore.js
+++ frontend/src/store/UserStore/userCategoryStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { axiosInstance } from "../../utils/axios.js";
+import { axiosInstance } from "../../utils/axios.jsx";
 
 export const userCategoryStore = create((set, get) => ({
   userCategories: [],
